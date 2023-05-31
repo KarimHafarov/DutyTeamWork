452e3acc47a6017aea5643fab4548c4f4ee47cf6
Karim <krmgafarov@gmail.com>
2023-05-31 10:08:56 +0300

    start project

---
+++ src/test/java/com/example/Duty/DutyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Duty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
